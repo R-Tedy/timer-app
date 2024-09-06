838c270a8d62b65db96a96cd9bf48887bfc9326b
Roland Stedy <stedyroland@gmail.com>
2024-09-06 04:44:09 +0300

    done and dusted... a quick leston on patience...

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hoursView = document.getElementById('hours');
 const minsView = document.getElementById('mins');
 const secsView = document.getElementById('secs');
-const stop = document.getElementById('stop');
+const stopIt = document.getElementById('stop');
 const play = document.getElementById('play');
 const restart = document.getElementById('restart')
 
@@ -9,16 +9,53 @@ let secs = 0;
 let mins = 0;
 let hours = 0;
 
-play.addEventListener('click',stopWatch)
+let timer = null;
+
+play.addEventListener('click',runWatch);
+stopIt.addEventListener('click',stopTimer);
+restart.addEventListener('click',restartTimer);
+
 
 function  stopWatch(){
-  if (secs < 60) {
-    secs++;
-    secsView.innerText = secs;
-  } else if (secs = 60){
+  let h = hours < 10 ? '0' + hours : hours;
+  let m = mins < 10 ? '0' + mins : mins;
+  let s = secs < 10 ? '0' + secs : secs;
+
+  secs++;
+  
+  if (secs == 60) {
     secs = 0;
     mins++;
-    minsView.innerText = mins;
-    inter
+    if (mins == 60) {
+      mins = 0;
+      hours++;
+    }
   }
+  minsView.innerHTML = m;
+  secsView.innerHTML = s;
+  hoursView.innerHTML = h;
+}
+
+function runWatch() {
+  if (timer != null) {
+    clearInterval(timer)
+  } else{
+    timer = setInterval(stopWatch,1000)
+  }
+}
+
+console.log(secs)
+
+function stopTimer() {
+  clearInterval(timer)
+}
+
+function restartTimer(){
+  clearInterval(timer);
+  hours = 0;
+  mins = 0;
+  secs = 0;
+  minsView.innerHTML = '00';
+  secsView.innerHTML = '00';
+  hoursView.innerHTML = '00';
 }
